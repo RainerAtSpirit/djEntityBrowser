6b8e6a905e0927583202f36994dd353917878eef
RainerAtSpirit <R.Wittmann@spirit.de>
2013-09-01 14:28:28 +0200

    add datajs 1.1.1 source

--- app/entities/index.js
+++ app/entities/index.js
@@ -1,4 +1,4 @@
-﻿define(['durandal/system', 'plugins/router', 'knockout', 'global'], function (system, router, ko, global) {
+﻿define(['durandal/system', 'plugins/router', 'knockout', 'global'], function( system, router, ko, global ) {
 
     var runOnce = true;
     var entities = ko.observableArray([]);
@@ -8,17 +8,17 @@
             moduleId: 'entities',
             fromParent: true
         }).map([
-                // Home view
-                { route: '', moduleId: 'home', title: 'Hello and welcome', nav: true },
+            // Home view
+            { route: '', moduleId: 'home', title: 'Hello and welcome', nav: true },
 
-                // Single entity view
-                { route: 'browse/:entities', moduleId: 'entity', title: 'Entity browser' },
+            // Single entity view
+            { route: 'browse/:entities', moduleId: 'entity', title: 'Entity browser' },
 
-                // ... filtered by navigation parameter
-                { route: 'browse/:entities/:relatedEntity/:relateId', moduleId: 'entity', title: 'Entity browser' },
+            // ... filtered by navigation parameter
+            { route: 'browse/:entities/:relatedEntity/:relateId', moduleId: 'entity', title: 'Entity browser' },
 
-                // Item view
-                { route: 'item/:item/:entityId', moduleId: 'item', title: 'Item view' }
+            // Item view
+            { route: 'item/:item/:entityId', moduleId: 'item', title: 'Item view' }
         ]).buildNavigationModel();
 
     return {
@@ -27,55 +27,55 @@
         activate: activate
     };
 
-    function activate() {
-        if (runOnce) {
+    function activate () {
+        if ( runOnce ) {
             runOnce = false;
-            global.config.oDataURI.subscribe(function (value) {
+
+            global.config.oDataURI.subscribe(function( value ) {
                 createEntityMap();
             });
         }
 
-        // Enable JSONP if service doesn't support CORS
-        OData.defaultHttpClient.enableJsonpCallback = !global.config.oDataURI().cors;
-
         return createEntityMap();
-        
+
     }
-    
-    function createEntityMap() {
+
+    function createEntityMap () {
         var oDataURI = global.config.oDataURI();
 
-        if (global.ctx[oDataURI.id]) {
+        // Enable JSONP if service doesn't support CORS
+        OData.defaultHttpClient.enableJsonpCallback = !oDataURI.cors;
+
+        if ( global.ctx[oDataURI.id] ) {
             entities(global.entityMaps[oDataURI.id]);
             return;
         }
 
         return $data.initService(oDataURI.url)
-        .then(function (context) {
-            var entityMap = [];
-            global.ctx[oDataURI.id] = context;
+            .then(function( context ) {
+                var entityMap = [];
+                global.ctx[oDataURI.id] = context;
 
+                //context.onReady({
+                //    error: function (exception) {
+                //        system.log('Context:Error', exception);
+                //    }
+                //});
 
-            //context.onReady({
-            //    error: function (exception) {
-            //        system.log('Context:Error', exception);
-            //    }
-            //});
-
-            global.ctx[oDataURI.id].forEachEntitySet(function (entity) {
-                entityMap.push({
-                    hash: '#entities/browse/' + encodeURIComponent(entity.name),
-                    title: entity.name,
-                    isActive: ko.computed(function () {
-                        var i = router.activeInstruction();
-                        return i && ko.utils.arrayIndexOf(i.params, '/browse/' + entity.name) !== -1;
-                    })
+                global.ctx[oDataURI.id].forEachEntitySet(function( entity ) {
+                    entityMap.push({
+                        hash: '#entities/browse/' + encodeURIComponent(entity.name),
+                        title: entity.name,
+                        isActive: ko.computed(function() {
+                            var i = router.activeInstruction();
+                            return i && ko.utils.arrayIndexOf(i.params, '/browse/' + entity.name) !== -1;
+                        })
+                    });
                 });
-            });
 
-            global.entityMaps[oDataURI.id] = entityMap;
-            entities(entityMap);
-        });
+                global.entityMaps[oDataURI.id] = entityMap;
+                entities(entityMap);
+            });
     }
 
 });
